b95ae66adfb2eac6f30b1f0c87da79c62304b07a
qlh <708937822@qq.com>
2018-02-08 16:33:02 +0800

    让拍照时预览图和最终存储的图像一致

--- app/src/main/java/com/cymaybe/foucssurfaceview/activity/MainActivity.java
+++ app/src/main/java/com/cymaybe/foucssurfaceview/activity/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.graphics.PixelFormat;
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -17,31 +19,45 @@ import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.blankj.utilcode.util.ImageUtils;
+import com.blankj.utilcode.util.ScreenUtils;
+import com.blankj.utilcode.util.ToastUtils;
 import com.cymaybe.foucssurfaceview.R;
 import com.cymaybe.foucssurfaceview.fragment.PictureFragment;
 import com.cymaybe.foucsurfaceview.FocusSurfaceView;
+import com.tbruyelle.rxpermissions2.RxPermissions;
+
+import io.reactivex.functions.Action;
+import io.reactivex.functions.Consumer;
 
 import static android.Manifest.permission.CAMERA;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 import static com.cymaybe.foucssurfaceview.fragment.PictureFragment.CROP_PICTURE;
 import static com.cymaybe.foucssurfaceview.fragment.PictureFragment.ORIGIN_PICTURE;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, SurfaceHolder.Callback {
-    private static final String TAG = "moubiao";
 
+    private static final String TAG = "wzgl5533";
+    private MainActivity instance;
     private FocusSurfaceView previewSFV;
     private Button mTakeBT, mThreeFourBT, mFourThreeBT, mNineSixteenBT, mSixteenNineBT, mFitImgBT, mCircleBT, mFreeBT, mSquareBT,
-            mCircleSquareBT, mCustomBT,mChange;
+            mCircleSquareBT, mCustomBT;
 
     private Camera mCamera;
+    private int cameraPosition = Camera.CameraInfo.CAMERA_FACING_BACK;//1:采集指纹的前置摄像头. 0:拍照的后置摄像头.
     private SurfaceHolder mHolder;
     private boolean focus = false;
+    private RxPermissions rxPermissions;
+    private boolean isGranted = false;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        instance = this;
         initData();
         initView();
         setListener();
@@ -50,6 +66,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void initData() {
         DetectScreenOrientation detectScreenOrientation = new DetectScreenOrientation(this);
         detectScreenOrientation.enable();
+        rxPermissions = new RxPermissions(this);
     }
 
     private void initView() {
@@ -69,7 +86,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mSquareBT = (Button) findViewById(R.id.square_bt);
         mCircleSquareBT = (Button) findViewById(R.id.circle_square_bt);
         mCustomBT = (Button) findViewById(R.id.custom_bt);
-        mChange = (Button) findViewById(R.id.change_c);
+
+        previewSFV.setTopTipText("wzgl5533");
     }
 
     private void setListener() {
@@ -84,51 +102,49 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mSquareBT.setOnClickListener(this);
         mCircleSquareBT.setOnClickListener(this);
         mCustomBT.setOnClickListener(this);
-        mChange.setOnClickListener(this);
     }
 
     @Override
     public void surfaceCreated(SurfaceHolder surfaceHolder) {
         initCamera();
-        setCameraParams();
     }
 
     private void initCamera() {
-        if (checkPermission()) {
-            try {
-                mCamera = android.hardware.Camera.open(0);//1:采集指纹的摄像头. 0:拍照的摄像头.
-                mCamera.setPreviewDisplay(mHolder);
-            } catch (Exception e) {
-                Snackbar.make(mTakeBT, "camera open failed!", Snackbar.LENGTH_SHORT).show();
-                finish();
-                e.printStackTrace();
-            }
-        } else {
-            requestPermission();
-        }
-    }
+        rxPermissions.request(CAMERA)
+                .subscribe(new Consumer<Boolean>() {
+                               @Override
+                               public void accept(Boolean granted) throws Exception {
+                                   if (granted) {
+                                       Log.e(TAG, "granted");
+                                       try {
+                                           mCamera = Camera.open(cameraPosition);//1:采集指纹的前置摄像头. 0:拍照的后置摄像头.
+                                           mCamera.setPreviewDisplay(mHolder);
+                                           setCameraParams();
+                                           isGranted = true;
+                                       } catch (Exception e) {//5.0权限拒绝或者不选，都走异常
+                                           ToastUtils.showShort("摄像头或存储权限被拒绝,请到权限管理中设置");
+                                           finish();
+                                           e.printStackTrace();
+                                       }
+                                   } else {
+                                       ToastUtils.showShort("摄像头或存储权限被拒绝,请到权限管理中设置");
+                                       finish();
+                                   }
 
-    private boolean checkPermission() {
-        return ContextCompat.checkSelfPermission(getApplicationContext(), CAMERA) == PackageManager.PERMISSION_GRANTED;
-    }
-
-    private void requestPermission() {
-        ActivityCompat.requestPermissions(this, new String[]{CAMERA}, 10000);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case 10000:
-                if (grantResults.length > 0) {
-                    if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                        initCamera();
-                        setCameraParams();
-                    }
-                }
-
-                break;
-        }
+                               }
+                           },
+                        new Consumer<Throwable>() {
+                            @Override
+                            public void accept(Throwable throwable) throws Exception {
+                                Log.e(TAG, "onError", throwable);
+                            }
+                        },
+                        new Action() {
+                            @Override
+                            public void run() throws Exception {
+                                Log.i(TAG, "OnComplete");
+                            }
+                        });
     }
 
     private void setCameraParams() {
@@ -137,24 +153,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
         try {
             Camera.Parameters parameters = mCamera.getParameters();
-
-            int orientation = judgeScreenOrientation();
-            if (Surface.ROTATION_0 == orientation) {
-                mCamera.setDisplayOrientation(90);
-                parameters.setRotation(90);
-            } else if (Surface.ROTATION_90 == orientation) {
-                mCamera.setDisplayOrientation(0);
-                parameters.setRotation(0);
-            } else if (Surface.ROTATION_180 == orientation) {
-                mCamera.setDisplayOrientation(180);
-                parameters.setRotation(180);
-            } else if (Surface.ROTATION_270 == orientation) {
-                mCamera.setDisplayOrientation(180);
-                parameters.setRotation(180);
+            Camera.CameraInfo info = new Camera.CameraInfo();
+            Camera.getCameraInfo(cameraPosition, info);
+            int rotation = judgeScreenOrientation();
+            int degrees = 0;
+            switch (rotation) {
+                case Surface.ROTATION_0:
+                    degrees = 0;
+                    break;
+                case Surface.ROTATION_90:
+                    degrees = 90;
+                    break;
+                case Surface.ROTATION_180:
+                    degrees = 180;
+                    break;
+                case Surface.ROTATION_270:
+                    degrees = 270;
+                    break;
             }
 
-            parameters.setPictureSize(1280, 720);
-            parameters.setPreviewSize(1280, 720);
+            int result;
+            if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+                result = (info.orientation + degrees) % 360;
+                result = (360 - result) % 360;
+            } else {
+                result = (info.orientation - degrees + 360) % 360;
+            }
+            mCamera.setDisplayOrientation(result);
+            //parameters.setRotation(result);
+
+            parameters.setPictureFormat(PixelFormat.JPEG);
+            parameters.setPictureSize(ScreenUtils.getScreenHeight(), ScreenUtils.getScreenWidth());
+            parameters.setPreviewSize(ScreenUtils.getScreenHeight(), ScreenUtils.getScreenWidth());
             mCamera.setParameters(parameters);
             mCamera.startPreview();
         } catch (Exception e) {
@@ -162,6 +192,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    //切换摄像头(备用)
+    private void changeCameraOrientation() {
+        Camera.CameraInfo cameraInfo = new Camera.CameraInfo();
+        Camera.getCameraInfo(cameraPosition, cameraInfo);
+        mCamera.stopPreview();//停掉原来摄像头的预览
+        mCamera.release();//释放资源
+        mCamera = null;//取消原来摄像头
+        if (cameraInfo.facing == Camera.CameraInfo.CAMERA_FACING_BACK) {//此时后置
+            Log.i(TAG, cameraPosition + "BACK");
+            cameraPosition = Camera.CameraInfo.CAMERA_FACING_FRONT;
+
+        } else {//此时前置
+            Log.i(TAG, cameraPosition + "For");
+            cameraPosition = Camera.CameraInfo.CAMERA_FACING_BACK;
+        }
+        initCamera();
+        setCameraParams();
+    }
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
         View decorView = getWindow().getDecorView();
@@ -169,6 +217,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    }
+
+    //判断是否授权
+    private boolean isGranted() {
+
+        return isGranted;
     }
 
     /**
@@ -202,12 +256,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.take_bt:
-                if (!focus) {
+                if (isGranted()) {
                     takePicture();
+                } else {
+                    ToastUtils.showShort("摄像头权限未开启，不能拍照");
                 }
-                break;
-            case R.id.change_c:
-                previewSFV.setTopTipText("monou");
                 break;
             case R.id.three_four_bt:
                 previewSFV.setCropMode(FocusSurfaceView.CropMode.RATIO_3_4);
@@ -245,7 +298,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     /**
-     * 拍照
+     * 拍照,（前置摄像头目前在三星某些手机上并不能自动获取焦点，拍照失效），可以先把mCamera.autoFocus去掉，直接拍照，本demo使用后置的演示
      */
     private void takePicture() {
         mCamera.autoFocus(new Camera.AutoFocusCallback() {
@@ -261,16 +314,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     }, null, null, new Camera.PictureCallback() {
                         @Override
                         public void onPictureTaken(byte[] data, Camera camera) {
-                            Bitmap originBitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
-                            Bitmap cropBitmap = previewSFV.getPicture(data);
+                            Bitmap originalBitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+                            Log.e(TAG, originalBitmap.getWidth()+"--original--"+ originalBitmap.getHeight());
+                            //前置图片顺时针旋转180度加镜像翻转
+                            if (cameraPosition == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+                                originalBitmap = mirrorRotate(rotate(originalBitmap,180));
+                            }
+
+                            //目前本人测试的几款手机都会有90度的旋转，所以需要复原图片，如果有不同之处，请在github留言
+                            if (cameraPosition == Camera.CameraInfo.CAMERA_FACING_BACK){
+                                originalBitmap = rotate(originalBitmap,90);
+                            }else {
+                                originalBitmap = rotate(originalBitmap,-90);
+                            }
+                            //到此图片和预览视角相同
+                            Log.e(TAG, originalBitmap.getWidth()+"----"+ originalBitmap.getHeight());
+                            Bitmap cropBitmap = previewSFV.getCropPicture(originalBitmap);
+
                             PictureFragment pictureFragment = new PictureFragment();
                             Bundle bundle = new Bundle();
-                            bundle.putParcelable(ORIGIN_PICTURE, originBitmap);
+                            bundle.putParcelable(ORIGIN_PICTURE, originalBitmap);
                             bundle.putParcelable(CROP_PICTURE, cropBitmap);
                             pictureFragment.setArguments(bundle);
                             pictureFragment.show(getFragmentManager(), null);
 
-                           Log.e("11111",cropBitmap.getWidth()+"----"+cropBitmap.getHeight());
+                           Log.e(TAG,cropBitmap.getWidth()+"--crop--"+cropBitmap.getHeight());
                             focus = false;
                             mCamera.startPreview();
                         }
@@ -296,5 +364,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 setCameraParams();
             }
         }
+    }
+
+    /**前置图片顺时针旋转180度**/
+    private Bitmap rotate(Bitmap bitmap, int degree){
+
+        return ImageUtils.rotate(bitmap, degree,
+                bitmap.getWidth() / 2, bitmap.getHeight() / 2, true);
+    }
+    /**图片镜像翻转**/
+    private Bitmap mirrorRotate(Bitmap bitmap){
+        Matrix matrix = new Matrix();
+        matrix.postScale(-1, 1);// 镜像水平翻转
+        return Bitmap.createBitmap(bitmap, 0, 0,
+                bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        instance = null;
     }
 }
